7ba89a850c0efbac695c4f03fa7175efda96b0c5
Aleysha Ukonu <aleysha2000@outlook.com>
2020-06-04 21:30:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 board = ["O", "X", "X", "X", "X", " ", " ", " ", " "]
 def display_board(board)
-  puts " #{board[1]} | #{board[2]} | #{board[3]}"
+  puts "   |   |   "
   puts "-----------"
-  puts "   | #{board[4]} |   "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
