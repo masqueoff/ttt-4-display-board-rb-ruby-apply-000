4feed11d67975ed103c20e6998415d3550f4df06
Aleysha Ukonu <aleysha2000@outlook.com>
2020-06-04 21:09:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board (board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   | X |   "
   puts "-----------"
   puts "   |   |   "
 end
-board = [" "," "," "," ","X"," "," "," "," "]
+board = [" "," "," "," ",""," "," "," "," "]
 display_board (board)
